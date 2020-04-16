0ba16823d944ce3457c1679b0dbcdbdd0b8a88d0
Teagan Rett <teagan.rett@gmail.com>
2020-04-16 13:29:38 -0500

    Initial sharing of project

---
+++ Card.java
@@ -0,0 +1,11 @@
+;public class Card
+{
+    private int cardNum;
+    private String cardType;
+    
+    public Card(int inNum, String inType)
+    {
+        cardNum = inNum;
+        cardType = inType;
+    }
+}
---
+++ DeckOfCards.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class DeckOfCards
+{
+    private ArrayList<Card> deck;
+    public DeckOfCards()
+    {
+        deck = new ArrayList<> ();
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 0; j++)
+            {
+            }
+        }
+    }
+}
